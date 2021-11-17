7f0a15779fdf906db19922082fa3bce7bdc1ea12
Jack1eAW <59226187+Jack1eAW@users.noreply.github.com>
2021-11-17 14:39:48 +0300

    Initial Commit

---
+++ NewsFeedApp/NewsFeedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedAppApp.swift
+//  NewsFeedApp
+//
+//  Created by Aleksandr Chebotarev on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
